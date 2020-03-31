9ff9305ebb1fccd1f431da2b04ae9ef640529102
Diegochits <54562062+dhna32@users.noreply.github.com>
2020-03-30 19:17:31 -0600

    Finished T Shirt Section
    No coments.

--- js/script.js
+++ js/script.js
@@ -23,3 +23,65 @@ select.addEventListener('click', () => {
 
 
 /** T SHIRT INFO **/
+ 
+/*Hides the Select Theme option */
+const selectTheme = document.querySelector('option[value="select theme"]');
+selectTheme.hidden = true;
+/*Selects the text content of the Color label*/
+const colorLabel = document.querySelector('label[for="color"]');
+colorLabel.textContent = 'Please select a T-shirt theme'
+
+const colors = document.querySelector('#color');
+colors.hidden = true; /*Hides drop down menu*/
+ 
+/*Hides all the color options from drop down menu*/ 
+const cornflowerBlue = document.querySelector('option[value="cornflowerblue"]');
+cornflowerBlue.hidden = true;
+
+const darkslategrey = document.querySelector('option[value="darkslategrey"]');
+darkslategrey.hidden = true;
+
+const gold = document.querySelector('option[value="gold"]');
+gold.hidden = true;
+
+const tomato = document.querySelector('option[value="tomato"]');
+tomato.hidden = true;
+
+const steelblue = document.querySelector('option[value="steelblue"]');
+steelblue.hidden = true;
+
+const dimgrey = document.querySelector('option[value="dimgrey"]');
+dimgrey.hidden = true;
+
+/*Selects the select-design section*/ 
+const design = document.querySelector('#design');
+/*Sets a listener for changes in the option selection, depending on the 
+  selections certain options will be appearing.
+*/
+design.addEventListener('change', (event) =>{
+ 
+   if(event.target.value === 'js puns'){ 
+    colors.hidden = false;
+    colors.value = "cornflowerblue";
+    cornflowerBlue.hidden = false;
+    darkslategrey.hidden = false;
+    gold.hidden = false;
+    tomato.hidden = true;
+    steelblue.hidden = true;
+    dimgrey.hidden = true;
+   } else if(event.target.value === 'heart js') {
+    colors.hidden = false;
+    colors.value = "tomato";
+    tomato.hidden = false;
+    steelblue.hidden = false;
+    dimgrey.hidden = false;
+    colors.hidden = false;
+    cornflowerBlue.hidden = true;
+    darkslategrey.hidden = true;
+    gold.hidden = true;
+   }
+});
+
+
+/*ACTIVITY SECTION*/
+
